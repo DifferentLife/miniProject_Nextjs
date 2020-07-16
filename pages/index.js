7aa33b0fb16e82d6f102afb2b116bd8bb0b415da
DifferentLife <differentlife0725@gmail.com>
2020-07-16 23:21:40 +0700

    twenty-two

---
+++ pages/index.js
@@ -0,0 +1,29 @@
+import Head from 'next/head'
+import { Layout, Menu, } from 'antd'
+
+const { Header } = Layout
+const Index = () => {
+  return (
+    <div>
+      <Head>
+        <title>Create Next App</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <Layout>
+        {/* <Header> */}
+          <Menu mode="horizontal" theme="dark">
+            <Menu.Item key="product">
+              <h3>Product</h3>
+            </Menu.Item>
+            <Menu.Item key="About">
+              <h3>About</h3>
+            </Menu.Item>
+          </Menu>
+        {/* </Header> */}
+      </Layout>
+
+    </div>
+  )
+}
+
+export default Index
